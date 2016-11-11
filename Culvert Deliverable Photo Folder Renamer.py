2d2fc613ee3ea826d475ec635040c07019c14f6d
dbgmcclelland <dbgmcclelland@gmail.com>
2016-11-10 22:19:00 -0500

    Added scripts.html

--- Culvert Deliverable Photo Folder Renamer.py
+++ Culvert Deliverable Photo Folder Renamer.py
@@ -1,23 +1,25 @@
-import arcpy, shutil, os
+import arcpy, os
  
-#Set File Locations for culverts feature class
+# Set File Locations for culverts feature class and create list of required fields
 Culverts_FC = r"W:\ENG\COMMON\Culvert Action Plan\GIS\Drainage Assets.gdb\Culverts_and_Sewers"
-#Culverts_FC = "W:\\ENG\\Geomatics\\GIS\\Map & Data Requests\\Contracts\\Maintenance\\Culverts\\Data\\2016\\Carillion\\Deliverable\\Carillion 2016 Culvert Inspection Deliverables.gdb\\Culverts_and_Sewers"
 fields = ["CVID", "DAID"]
 
-#For loop to cycle through features
+# Creating and populating list of IDs from folder names
 CVIDs = []
 rootDir = r"L:\ER Geomatics\Geomatics1\Culvert Inventory\4015-E-0008 A1\Assignment 1 Photos Folder 2\Assignment 1 Photos Folder 2"
+
 for item in os.listdir(rootDir):
         CVIDs.append(item)
 
 temp = "temp"
-arcpy.MakeFeatureLayer_management(Culverts_FC, temp)
+arcpy.MakeFeatureLayer_management(Culverts_FC, temp) # Temporary layer required to use arcpy selctions
 
+# Creates selection from list of IDs in folder
 for ID in CVIDs:
         query = "CVID = '" + ID + "'"
         arcpy.SelectLayerByAttribute_management(temp, "ADD_TO_SELECTION", query)
 
+# Search cursor for loop that matches old ID to new ID, and then renames the subfolder
 cursor = arcpy.SearchCursor(temp, fields)
 for row in cursor:
         CVID = row.getValue("CVID")
